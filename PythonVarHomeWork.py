5247e5393d3a9297f71df86c006645c3866e55b3
AndriyGulpa <46300383+AndriyGulpa@users.noreply.github.com>
2019-01-01 19:04:26 +0200

    Add files via upload

---
+++ PythonVarHomeWork.py
@@ -0,0 +1,19 @@
+#first coment
+"""
+Multi 
+line comment 
+dfg
+dfg
+"""
+
+Artist = "Patric Hernandes"
+song = "Born to be alive"
+year = 1979
+time = 3.17  
+Ganre = "Disco"
+print(Artist)
+print(song)
+print(year)
+print(time,"min")
+print(Ganre)
+ 
